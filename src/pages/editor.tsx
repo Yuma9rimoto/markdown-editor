83ef4a1ebe0ac0e7e631bff69fb9c9ac158309d9
YumaKurimotoDev <yuma.kurimoto.developer@gmail.com>
2022-12-19 20:47:29 +0900

    5章終了

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -1,14 +1,15 @@
 import * as React from "react";
 import styled from "styled-components";
 import { useStateWithStorage } from "../hooks/useStateWithStorage";
-import * as ReactMarkdown from "react-markdown";
 import { putMemo } from "../indexeddb/memos";
 import { Button } from "../components/button";
 import { SaveModal } from "../components/saveModal";
 import { Link } from "react-router-dom";
 import { Header } from "../components/header";
+import ConvertMarkdownWorker from "worker-loader!../worker/convertMarkdownWorker.ts";
 
-const { useState } = React;
+const convertMarkdownWorker = new ConvertMarkdownWorker();
+const { useState, useEffect } = React;
 
 const Wrapper = styled.div`
   bottom: 0;
@@ -48,12 +49,25 @@ const Preview = styled.div`
   width: 50vw;
 `;
 
-const StorageKey = "pages/editor:text";
+interface Props {
+  text: string;
+  setText: (text: string) => void;
+}
 
-export const Editor: React.FC = () => {
-  const [text, setText] = useStateWithStorage("", StorageKey);
-
+export const Editor: React.FC<Props> = (props) => {
+  const { text, setText } = props;
   const [showModal, setShowModal] = useState(false);
+  const [html, setHtml] = useState("");
+
+  useEffect(() => {
+    convertMarkdownWorker.onmessage = (event) => {
+      setHtml(event.data.html);
+    };
+  }, []);
+
+  useEffect(() => {
+    convertMarkdownWorker.postMessage(text);
+  }, [text]);
 
   return (
     <>
@@ -69,7 +83,7 @@ export const Editor: React.FC = () => {
           value={text}
         />
         <Preview>
-          <ReactMarkdown>{text}</ReactMarkdown>
+          <div dangerouslySetInnerHTML={{ __html: html }}>{text}</div>
         </Preview>
       </Wrapper>
       {showModal && (
